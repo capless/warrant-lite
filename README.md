4160d88d344b430a09b5154fc4e707899607ae85
Brian Jinwright <brian@ipoots.com>
2018-05-03 17:23:10 -0400

    added to the README and added setup.py

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # warrant-srp
-Python library for process SRP requests for AWS Cognito 
+Python library for process SRP requests for AWS Cognito. This library was initially included in the Warrant library. We decided to separate it because not all projects and workfows need all of the helper classes and functions in Warrant.
+
+## Install
+
+```python
+pip install warrant-srp
+```
---
+++ setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, find_packages
+
+
+def parse_requirements(filename):
+    """ load requirements from a pip requirements file """
+    lineiter = (line.strip() for line in open(filename))
+    return [line for line in lineiter if line and not line.startswith("#")]
+
+
+version = '1.0'
+
+README="""Python library for process SRP requests for AWS Cognito."""
+
+setup(
+    name='warrant-srp',
+    version=version,
+    description=README,
+    long_description=README,
+    classifiers=[
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.6",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Environment :: Web Environment",
+    ],
+    keywords='aws,cognito,srp,capless',
+    author='Capless.io',
+    author_email='opensource@capless.io',
+    maintainer='Brian Jinwright',
+    packages=find_packages(),
+    url='https://github.com/capless/warrant-srp',
+    license='Apache License 2.0',
+    install_requires=parse_requirements('requirements.txt'),
+    extras_require={
+        'test': parse_requirements('requirements_test.txt')
+    },
+    include_package_data=True,
+    zip_safe=True,
+
+)
