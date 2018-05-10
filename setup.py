531acdb25d319910e9fec79934bd853657857598
Brian Jinwright <brian@ipoots.com>
2018-05-10 11:15:20 -0400

    updated version 1.0.3

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def parse_requirements(filename):
     return [line for line in lineiter if line and not line.startswith("#")]
 
 
-version = '1.0.2'
+version = '1.0.3'
 
 README="""Small Python library for process SRP requests for AWS Cognito. This library was initially included in the [Warrant](https://www.github.com/capless/warrant) library. We decided to separate it because not all projects and workfows need all of the helper classes and functions in Warrant."""
 
